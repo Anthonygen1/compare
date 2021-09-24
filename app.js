8690b7d506e98541d0150347733920edbbcfdc5f
Anthony <agenova@groupiiico.com>
2021-09-24 11:02:16 -0400

    latest

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const app = Vue.createApp({
         return {
             listInput: '',
             listInputTwo: '',
-            listOne: [] ,
-            listTwo: []
+            sameSku: [],
+            differentSku: [],
+            doneComparing: false,
         };
     },
     computed: {
@@ -15,19 +16,28 @@ const app = Vue.createApp({
             e.preventDefault();
             const list = this.listInput.split(/[ .:;?!~,`"&|()<>{}\[\]\r\n/\\]+/);
             const listTwo = this.listInputTwo.split(/[ .:;?!~,`"&|()<>{}\[\]\r\n/\\]+/);
-            // console.log(list);
-            // console.log(listTwo);
             this.compareArray(list, listTwo);
+            this.doneComparing = true;
         },
         compareArray(a, b) {
             for (let i = 0; i < a.length; i++) {
                 const element = a[i];
                 b.filter(item => {
                     if(item === element) {
+                        this.sameSku.push(item);
                         console.log(item, element);
+                    } else if (item != element) {
+                        console.log (element, 'not the same')
                     }
                 })
             }
+        },
+        reload(e) {
+            e.preventDefault();
+            this.sameSku = [];
+            this.listInput = '';
+            this.listInputTwo = '';
+            this.doneComparing = false;
         }
     }
 });
